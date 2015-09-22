466d1bb61ada7065427033d79905c85235b263f1
mghthakkar <mghthakkar@gmail.com>
2015-09-22 16:40:02 +0530

    Add movies scaffold and users

---
+++ app/views/movies/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @movie, :id, :title, :description, :movie_length, :director, :created_at, :updated_at
---
+++ db/migrate/20150922103345_create_movies.rb
@@ -0,0 +1,12 @@
+class CreateMovies < ActiveRecord::Migration
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.text :description
+      t.string :movie_length
+      t.string :director
+
+      t.timestamps null: false
+    end
+  end
+end
