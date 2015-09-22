d43b7a3c9cd54e454e4fb51048dcdad5abd2daf5
mghthakkar <mghthakkar@gmail.com>
2015-09-22 17:03:29 +0530

    Add paperclip for image upload

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem 'coffee-script-source', ' 1.8.0'
 
 gem 'devise', '~> 3.5.2'
 
+gem 'paperclip', '~> 4.3.0'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,5 +41,6 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  
+  Paperclip.options[:command_path] = 'C:\Program Files\GnuWin32\bin'
+
 end
